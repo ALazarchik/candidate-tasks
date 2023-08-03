efa9fa809e72a3adb80785a3c08b816b4624b82e
Andrei <desto@mail.ru>
2023-08-03 11:35:44 +0300

    refactor login method

--- tasks/task01/task01.ts
+++ tasks/task01/task01.ts
@@ -9,15 +9,8 @@ const authenticationService = new AuthenticationService();
 
 class LoginPage {
     login(username: string, password: string, authService: AuthenticationService): boolean {
-        let result = false;
-
         // Call the authentication service to check the credentials
-        if (authService.authenticate(username, password)) {
-            // If the authentication service returns true, set the result to true
-            result = true;
-        }
-
-        return result;
+        return authService.authenticate(username, password);
     }
 }
 
